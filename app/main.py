2ac2626119d3a93d730f4119ea5d4f28db43239d
exl <levzavadskiy999@gmail.com>
2026-05-21 20:07:21 +0300

    feat: add routers in main

--- app/main.py
+++ app/main.py
@@ -1,8 +1,13 @@
 from fastapi import FastAPI
+from app.api.v1.devices import router as device_router
+from app.api.v1.users import router as user_router
 import uvicorn
 
 app = FastAPI()
 
+
+app.include_router(device_router)
+app.include_router(user_router)
 
 if __name__ == '__main__':
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
