eda00161c09ace1c7417992ff4323534d12bd47e
exl <levzavadskiy999@gmail.com>
2026-05-21 17:18:08 +0300

    feat: add models

---
+++ models/measurement.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+
+from sqlalchemy import ForeignKey, func
+from sqlalchemy.orm import Mapped, mapped_column
+
+from models.base import Base
+
+
+class MeasurementOrm(Base):
+    __tablename__ = "measurements"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    device_id: Mapped[int] = mapped_column(ForeignKey("devices.id"))
+    x: Mapped[float] = mapped_column()
+    y: Mapped[float] = mapped_column()
+    z: Mapped[float] = mapped_column()
+    timestamp: Mapped[datetime] = mapped_column(server_default=func.now())
